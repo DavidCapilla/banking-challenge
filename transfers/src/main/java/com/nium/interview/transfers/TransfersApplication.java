6b0d3240fee4b11f25fbcb56d9e6fc68d45e9a33
David Capilla <dcapillag@gmail.com>
2022-11-21 20:16:44 +0100

    dcg | Implement application

--- transfers/src/main/java/com/nium/interview/transfers/TransfersApplication.java
+++ transfers/src/main/java/com/nium/interview/transfers/TransfersApplication.java
@@ -1,6 +1,9 @@
 package com.nium.interview.transfers;
 
+import static java.util.Objects.nonNull;
+
 import com.nium.interview.transfers.domain.CvsTransferAdapter;
+import com.nium.interview.transfers.domain.Transfer;
 import com.nium.interview.transfers.domain.TransferAdapter;
 import com.nium.interview.transfers.statistics.AccountsSummariser;
 import java.io.IOException;
@@ -41,7 +44,23 @@ public class TransfersApplication implements CommandLineRunner {
     accountsSummariser = new AccountsSummariser();
 
     final URL file = getClass().getClassLoader().getResource("transfers.txt");
-    Files.readAllLines(Path.of(file.toURI()))
-        .forEach(line -> accountsSummariser.processData(transferAdapter.getTransfer(line)));
+    Files.readAllLines(Path.of(file.toURI())).forEach(this::processData);
+
+    System.out.println("#Balances");
+    accountsSummariser
+        .getAccounts()
+        .forEach(
+            account ->
+                System.out.println(
+                    account.getId() + " - " + String.format("%.2f", account.getBalance())));
+    System.out.println("#Bank Account with highest balance");
+    System.out.println(accountsSummariser.getAccountWithHigherBalance());
+    System.out.println("#Frequently used source bank account");
+    System.out.println(accountsSummariser.getMostUsedAccountAsSource());
+  }
+
+  private void processData(String line) {
+    Transfer transfer = transferAdapter.getTransfer(line);
+    if (nonNull(transfer)) accountsSummariser.processData(transfer);
   }
 }
